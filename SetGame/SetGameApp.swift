b661f5002ee30f3e45c0cfa2fab18b44ae9c951f
Krishna Kota <krishna.kota@appdynamics.com>
2021-07-13 16:15:29 +0530

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by krkota on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
